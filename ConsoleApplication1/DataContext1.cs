0f7559be2263b836310d688c225110b0efbd5d92
davidemoraschi <davidem@eurostrategy.net>
2016-08-23 14:49:18 +0200

    moved to GitHub

---
+++ ConsoleApplication1/DataContext1.cs
@@ -0,0 +1,10 @@
+// This file is intended to be edited manually
+
+namespace CrmDemoContext
+{
+    partial class CrmDemoDataContext
+    {
+
+        // Place your implementation of partial extension methods here
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CrmDemoContext;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CrmDemoDataContext context = new CrmDemoDataContext();
+            var query = from it in context.Companies
+                        orderby it.Companyid
+                        select it;
+
+            foreach (Company comp in query)
+            {
+                if (comp.Personcontacts.Count > 0)
+                {
+                    Console.WriteLine("{0} | {1} | {2}",
+                      comp.Companyname, comp.Personcontacts[0].Firstname,
+                      comp.Personcontacts[0].Lastname);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
